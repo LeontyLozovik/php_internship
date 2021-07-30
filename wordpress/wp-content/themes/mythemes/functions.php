530a39ed8cc9dcccae4427b5a991a12bc66a4fe1
Leontiy <56200371+LeontyLozovik@users.noreply.github.com>
2021-07-30 17:50:56 +0300

    Delete wordpress directory

--- wordpress/wp-content/themes/mythemes/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_action('wp_enqueue_scripts', 'plugin_styles');
-
-function plugin_styles() {
-    wp_enqueue_style('style', get_stylesheet_uri());
-    wp_enqueue_style('default', get_template_directory_uri() . '/assets/css/default.css');
-}
