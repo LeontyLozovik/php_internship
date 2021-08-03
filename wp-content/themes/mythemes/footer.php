b1079d81870d473b99f01ec31087847a00a70ec3
Leontiy Lozovik <leontyloz02@gmail.com>
2021-08-03 13:00:14 +0300

    update styles and header

--- wp-content/themes/mythemes/footer.php
+++ wp-content/themes/mythemes/footer.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Footer</title>
-    <link href="my_styles.css", rel="stylesheet", type="text/css">
+    <link href="assets/css/default.css", rel="stylesheet", type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
